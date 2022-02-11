3ca6f4d00f842e4b0a02a3d7747311100d31c903
Jelle Schrader <jelleschrader@gmail.com>
2022-02-11 15:22:23 +0100

    Add OrderServices

---
+++ App/Services/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+
+namespace App.Services;
+
+public interface IOrderService
+{
+    decimal CalculateOrderPrice(Order order);
+    Order GetOrderByCustomerName(string customerName);
+    List<Order> GetOrders();
+    void CreateOrder(Order order);
+    void UpdateOrder(Order order);
+    void AddProductToOrder(Order order, Product product);
+}
---
+++ App/Services/OrderService.cs
@@ -0,0 +1,53 @@
+﻿using App.Agents;
+using Models;
+
+namespace App.Services;
+
+public class OrderService : IOrderService
+{
+    private readonly IOrderAgent _orderAgent;
+
+    public OrderService(IOrderAgent orderAgent)
+    {
+        _orderAgent = orderAgent;
+    }
+
+    public void AddProductToOrder(Order order, Product product)
+    {
+        order.OrderDetails.Add(new OrderDetail() { Order = order, Product = product, TimeAdded = DateTime.Now });
+    }
+
+    public decimal CalculateOrderPrice(Order order)
+    {
+        decimal totalPrice = 0.00M;
+        if (order.IsMember)
+        {
+            order.OrderDetails.ForEach(od => totalPrice += od.Product.MemberPrice);
+        }
+        else
+        {
+            order.OrderDetails.ForEach(od => totalPrice += od.Product.Price);
+        }
+        return totalPrice;
+    }
+
+    public void CreateOrder(Order order)
+    {
+        _orderAgent.CreateOrder(order);
+    }
+
+    public Order GetOrderByCustomerName(string customerName)
+    {
+        return _orderAgent.GetOrderByName(customerName);
+    }
+
+    public List<Order> GetOrders()
+    {
+        return _orderAgent.GetOrders();
+    }
+
+    public void UpdateOrder(Order order)
+    {
+        _orderAgent.UpdateOrder(order);
+    }
+}
