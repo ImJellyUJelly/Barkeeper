62f27dafb5fcce31c5251134ab7e34f53e69e760
Jelle Schrader <jelle.schrader@capgemini.com>
2022-02-14 16:31:02 +0100

    Refactor font sizes, add split order screen

---
+++ App/Forms/SplitBestellingForm.cs
@@ -0,0 +1,18 @@
+﻿using App.Services;
+using Models;
+
+namespace App.Forms
+{
+    public partial class SplitBestellingForm : Form
+    {
+        private readonly IOrderService _orderService;
+        private readonly Order _order;
+
+        public SplitBestellingForm(IOrderService orderService, Order order)
+        {
+            _orderService = orderService;
+            _order = order;
+            InitializeComponent();
+        }
+    }
+}
