871b699186f37921d1c9c6730bf8040f677df045
Jelle Schrader <jelleschrader@gmail.com>
2022-03-01 20:44:15 +0100

    Remove constants

--- App/Constants/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace App.Constants;
-
-public static class Constants
-{
-    public const string CONNECTION_STRING = @"server=localhost;database=Barkeeper_Development;port=5432;uid=root;Password=password01!";
-}
-
